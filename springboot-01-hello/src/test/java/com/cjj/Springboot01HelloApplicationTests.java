367ce99cff66330a1cf165e490056ed02e59c278
cjjj123 <465313669@qq.com>
2020-03-28 14:08:40 +0800

    spring-boot-01-hello

---
+++ springboot-01-hello/src/test/java/com/cjj/Springboot01HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot01HelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
