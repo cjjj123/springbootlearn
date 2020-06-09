6a85b0abc285f6bea6a30402d05711d7816dad3f
cjjj123 <465313669@qq.com>
2020-06-09 23:59:38 +0800

    test

---
+++ springboot-07-data-jpa/src/test/java/com/cjj/Springboot07DataJpaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.cjj;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot07DataJpaApplicationTests {
+
+
+    void contextLoads() {
+    }
+
+}
